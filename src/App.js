2a366e42c673bee95f109ad1936f543d32c0bdfb
pochapali <ivantrufan27@gmail.com>
2022-09-12 11:42:22 +0300

    commit, lol :package:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [age, setAge] = useState(10);
+  const [HRL, setHRL] = useState();
+  function calculate() {
+    const result =
+      ((220 - age) * 0.65).toFixed(2) + "-" + ((220 - age) * 0.85).toFixed(2);
+    setHRL(result);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="HRL">
+      <header className="HRL calculator"></header>
+      <form>
+        <div>
+          <div>
+            <label>Age</label>
+          </div>
+          <input
+            type="number"
+            value={age}
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+          />
+          <div>
+            <label>HRL</label>
+          </div>
+          <output>{HRL}</output>
+          <div>
+            <button type="button" onClick={calculate}>
+              calculate
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
